48d9339c6fcf52a819f606d76a87a81f804c081d
Luu Thuong Hong <luuthuonghong@gmail.com>
2026-04-07 17:09:21 +0700

    Update MediaServiceImpl.java

--- media/src/main/java/com/yas/media/service/MediaServiceImpl.java
+++ media/src/main/java/com/yas/media/service/MediaServiceImpl.java
@@ -28,7 +28,7 @@ public class MediaServiceImpl implements MediaService {
     private final MediaVmMapper mediaVmMapper;
     private final MediaRepository mediaRepository;
     private final FileSystemRepository fileSystemRepository;
-    private final YasConfig yasConfig;//test4
+    private final YasConfig yasConfig;//test5
 
     @Override
     @SneakyThrows
