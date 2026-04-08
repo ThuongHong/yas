208f51455545ecf6a327dc11a8073b73b7459f6a
Luu Thuong Hong <luuthuonghong@gmail.com>
2026-04-08 17:54:25 +0700

    Update ProductSyncDataService.java

--- search/src/main/java/com/yas/search/service/ProductSyncDataService.java
+++ search/src/main/java/com/yas/search/service/ProductSyncDataService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@Service//testttttttt
+@Service//testtttttttt
 @RequiredArgsConstructor
 public class ProductSyncDataService {
 
