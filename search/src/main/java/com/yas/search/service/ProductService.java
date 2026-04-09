baa0fdc9a942c550d2bb227f1cc3f3a5f6982c5c
Luu Thuong Hong <luuthuonghong@gmail.com>
2026-04-09 08:25:42 +0700

    Update ProductService.java

--- search/src/main/java/com/yas/search/service/ProductService.java
+++ search/src/main/java/com/yas/search/service/ProductService.java
@@ -39,7 +39,7 @@ public class ProductService {
     public ProductService(ElasticsearchOperations elasticsearchOperations) {
         this.elasticsearchOperations = elasticsearchOperations;
     }
-// Test9 10
+// Test9 1011
     public ProductListGetVm findProductAdvance(ProductCriteriaDto productCriteria) {
         NativeQueryBuilder nativeQuery = NativeQuery.builder()
                 .withAggregation("categories", Aggregation.of(a -> a
